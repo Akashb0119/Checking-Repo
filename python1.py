373c6e420086a40d009a681bda2747a9b4fe437b
Duker Akash <akashnaveen679@gmail.com>
2026-06-20 13:24:09 +0530

    Update python1.py animation and add browser_demo.html

--- python1.py
+++ python1.py
@@ -1,2 +1,14 @@
+import sys
+import time
+
 print("Hello world")
 print("hello")
+print("this is my project -monish")
+
+message = "Monish is good developer"
+colors = [31, 33, 32, 36, 34, 35]
+for index, char in enumerate(message):
+    color_code = colors[index % len(colors)]
+    sys.stdout.write(f"\033[1;{color_code}m{char}\033[0m")
+    sys.stdout.flush()
+    time.sleep(0.05)
